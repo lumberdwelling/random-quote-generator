6030c57a7a4bd24fd19f61f28b1268381d2663b8
Patrick Koidal <pkoidal519@gmail.com>
2018-03-06 11:33:26 -0600

    update 2
    spinning shakespeare

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import shakespeare from './shakespeare-emoji.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={shakespeare} className="App-logo" alt="logo" />
+          <h1 className="App-title">Random Quote Generator</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button className="firstButton"> Click me for a random quote! </button>
       </div>
     );
   }
